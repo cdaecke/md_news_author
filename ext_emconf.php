0b8a9c59126478da9de353c5b119f8ece2b324bc
chris <typo3@mediadreams.org>
2017-05-03 10:58:05 +0200

    [TASK] compatibility with TYPO3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
   'uploadfolder' => '0',
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.0.0',
+  'version' => '1.0.1',
   'constraints' => array(
     'depends' => array(
-      'typo3' => '7.6.0-7.6.99',
+      'typo3' => '7.6.0-8.7.99',
       'news' => '4.0',
     ),
     'conflicts' => array(
