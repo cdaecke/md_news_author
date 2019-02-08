5de1abeff38d83d28606cb15066988cb85f23502
chris <typo3@mediadreams.org>
2019-02-08 22:17:58 +0100

    [TASK] lower dependency of ext:news version

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
   'constraints' => array(
     'depends' => array(
       'typo3' => '8.7.0-9.5.99',
-      'news' => '7.0',
+      'news' => '6.0',
     ),
     'conflicts' => array(
     ),
