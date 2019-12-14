ea4968febbae7fef03020f6d242dc9cc9fda4c83
chris <typo3@mediadreams.org>
2019-12-14 23:18:53 +0100

    [BUGFIX] get all news records on an authors detail page
    references #10

--- Classes/Domain/Repository/NewsRepository.php
+++ Classes/Domain/Repository/NewsRepository.php
@@ -35,21 +35,14 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 /**
  * The repository for News
  */
-class NewsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+class NewsRepository extends \GeorgRinger\News\Domain\Repository\NewsRepository
 {
   /**
-   * News table
+   * News author MM table
    *
    * @var string
    */
-  const TABLE_NEWS = 'tx_news_domain_model_news';
-
-  /**
-   * News author table
-   *
-   * @var string
-   */
-  const TABLE_AUTHOR = 'tx_mdnewsauthor_domain_model_newsauthor';
+  const TABLE_AUTHOR_MM = 'tx_mdnewsauthor_news_newsauthor_mm';
 
 
   // Ordering of result
@@ -68,72 +61,53 @@ class NewsRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
 
   /**
    * Find news by authors uid
+   * We use this to get the news records ordered by "datetime"
    *
    * @param int $authorUid Uid of author
-   * @return array
+   * @return obj | false
    */
-  public function getNewsByAuthor($authorUid)
+  public function getNewsByAuthor(int $authorUid)
   {
-    // Initialize Query Builder for table 'tx_mdnewsauthor_domain_model_newsauthor'
+    // Initialize Query Builder for table 'tx_mdnewsauthor_news_newsauthor_mm'
     /** @var QueryBuilder $queryBuilderNews */
-    $queryBuilderAuthor = GeneralUtility::makeInstance(ConnectionPool::class)
-                          ->getQueryBuilderForTable(self::TABLE_AUTHOR);
+    $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+                          ->getQueryBuilderForTable(self::TABLE_AUTHOR_MM);
 
-
-    // look for translated author and overwrite $authorUid
-    if ($GLOBALS['TSFE']->sys_language_uid > 0) {
-      $translatedAuthor = $queryBuilderAuthor
-        ->select('uid')
-        ->from(self::TABLE_AUTHOR)
-        ->where(
-            $queryBuilderAuthor->expr()->eq(
-              'sys_language_uid',
-              $queryBuilderAuthor->createNamedParameter((int)$GLOBALS['TSFE']->sys_language_uid, \PDO::PARAM_INT)
-            )
-          )
-        ->andWhere(
-            $queryBuilderAuthor->expr()->eq(
-              'l10n_parent', 
-              $queryBuilderAuthor->createNamedParameter((int)$authorUid, \PDO::PARAM_INT)
-            )
-         )
-        ->setMaxResults(1)
-        ->execute()
-        ->fetchAll();
-
-      if ($translatedAuthor && $translatedAuthor[0]['uid']) {
-        $authorUid = $translatedAuthor[0]['uid'];
-      }
-    }
-
-
-    // Initialize Query Builder for table 'tx_news_domain_model_news'
-    /** @var QueryBuilder $queryBuilderNews */
-    $queryBuilderNews = GeneralUtility::makeInstance(ConnectionPool::class)
-                        ->getQueryBuilderForTable(self::TABLE_NEWS);
-
-    $news = $queryBuilderNews
-      ->select('tx_news_domain_model_news.*')
-      ->from(self::TABLE_NEWS)
-      ->leftJoin(
-        self::TABLE_NEWS,
-        self::TABLE_AUTHOR,
-        'newsauthorMM',
-        $queryBuilderNews->expr()->eq(
-          'newsauthorMM.uid', 
-          $queryBuilderNews->quoteIdentifier('tx_news_domain_model_news.uid')
-        )
-      )
+    // find all news IDs which belong to the author
+    $newsUids = $queryBuilder
+      ->select('uid_local')
+      ->from(self::TABLE_AUTHOR_MM)
       ->where(
-        $queryBuilderNews->expr()->eq(
-          'newsauthorMM.uid', 
-          $queryBuilderNews->createNamedParameter((int)$authorUid, \PDO::PARAM_INT)
+          $queryBuilder->expr()->eq(
+            'uid_foreign',
+            $queryBuilder->createNamedParameter((int)$authorUid, \PDO::PARAM_INT)
+          )
         )
-      )
       ->execute()
       ->fetchAll();
 
-    return $news;
+    // build array with news IDs
+    $newsArr = [];
+    foreach ($newsUids as $newsUid) {
+      $newsArr[] = $newsUid['uid_local'];
+    }
+    
+    // finally get the news records
+    if ( count($newsArr) > 0 ) {
+      $constraint = [];
+      $query = $this->createQuery();
+
+      $constraints[] = $query->in('uid', $newsArr);
+
+      $query->matching(
+        $query->logicalAnd($constraints)
+      );
+
+      return $query->execute();
+    } else {
+      return false;
+    }
+
   }
-  
+
 }
