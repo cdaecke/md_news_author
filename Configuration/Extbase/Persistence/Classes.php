d15e921071e91a96a41b7df6640cad45fc873632
chris <typo3@mediadreams.org>
2020-07-31 12:26:12 +0200

    [TASK] add Extbase/Persistence/Classes.php configuration

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types = 1);
+
+return [
+    Mediadreams\MdNewsAuthor\Domain\Model\News::class => [
+        'tableName' => 'tx_news_domain_model_news',
+        'recordType' => 0,
+    ],
+];
