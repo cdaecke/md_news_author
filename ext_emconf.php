5925055d429f2c3fa871de87a5fa9bb95d93d06f
chris <typo3@mediadreams.org>
2020-08-04 23:14:16 +0200

    [TASK] raise version number to 5.0.0 and update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,11 +21,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '4.0.1',
+    'version' => '5.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
-            'news' => '6.0',
+            'typo3' => '9.5.0-10.4.99',
+            'news' => '7.0-8.99',
         ],
         'conflicts' => [],
         'suggests' => [],
