8a06a1abc43a70147fa35bec47786b2aeddafcbb
chris <typo3@mediadreams.org>
2026-04-09 08:55:05 +0200

    [TASK] tighten some potential security issues: escape auhtor names, before translating to prevent XSS attacs. `setRespectStoragePage(false)` not globally but only for method `getNewsByAuthor()` in the `NewsRepository`

--- Classes/Domain/Repository/NewsRepository.php
+++ Classes/Domain/Repository/NewsRepository.php
@@ -49,15 +49,6 @@ class NewsRepository extends \GeorgRinger\News\Domain\Repository\NewsRepository
         'datetime' => QueryInterface::ORDER_DESCENDING,
     ];
 
-    public function createQuery()
-    {
-        $query = parent::createQuery();
-        $settings = $query->getQuerySettings();
-        $settings->setRespectStoragePage(false);
-        $query->setQuerySettings($settings);
-        return $query;
-    }
-
     /**
      * Find news by authors uid
      * We use this to get the news records ordered by "datetime"
@@ -67,12 +58,12 @@ class NewsRepository extends \GeorgRinger\News\Domain\Repository\NewsRepository
     public function getNewsByAuthor(int $authorUid): QueryResultInterface
     {
         $query = $this->createQuery();
+        // Intentionally ignore storage page: an author's news may be stored across
+        // multiple sysfolders, so we fetch them all regardless of storagePid.
+        $query->getQuerySettings()->setRespectStoragePage(false);
         $query->matching(
             $query->logicalAnd(
-                ...
-                [
-                    $query->equals('newsAuthor.uid', $authorUid),
-                ]
+                $query->equals('newsAuthor.uid', $authorUid),
             )
         );
 
